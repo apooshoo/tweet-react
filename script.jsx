5dc8d9542c14a893526835c62ebabd149475b1e9
Jonathan <jonathanbek93@gmail.com>
2019-08-28 01:23:44 +0800

    rendered name and text

--- script.jsx
+++ script.jsx
@@ -1,7 +1,37 @@
-var monkey = "hello";
+//render user and entities
+//user:
+//name, screenname, howlongago, profile_banner_url
+class TweetName extends React.Component{
+    render(){
+        return(
+            <p>{this.props.tweet.user.name}</p>
+    )};
+};
+class TweetText extends React.Component{
+    render(){
+        console.log(this.props.tweet)
+        return(
+            <p>{this.props.tweet.text}</p>
+    )};
+};
+
+class TweetList extends React.Component{
+    render(){
+        let tweet = this.props.tweets.map((tweet, index) => {
+            return(
+            <div>
+                <TweetName tweet={tweet}/>
+                <TweetText tweet={tweet}/>
+            </div>
+        )});
+        return(
+            <div>{tweet}</div>
+    )};
+};
+
 ReactDOM.render(
     <div>
-      {monkey}
+        <TweetList tweets={tweets}/>
     </div>,
     document.getElementById('root')
 );
