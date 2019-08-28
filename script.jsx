0690dd879a1e8fd3f67ba881228f8525a8814583
Jonathan <jonathanbek93@gmail.com>
2019-08-28 08:20:30 +0800

    did list, started a little on user details but stopped

--- script.jsx
+++ script.jsx
@@ -1,17 +1,61 @@
 //render user and entities
 //user:
 //name, screenname, howlongago, profile_banner_url
-class TweetName extends React.Component{
+class UserHeader extends React.Component{
     render(){
         return(
-            <p>{this.props.tweet.user.name}</p>
+            <div>
+                <p>{this.props.user.screen_name}</p>
+                <p>{this.props.user.statuses_count}</p>
+            </div>
+    )};
+};
+
+class UserBanner extends React.Component{
+    render(){
+        return(
+            <div>
+                <img src={this.props.user.profile_banner_url}/>
+            </div>
+    )};
+};
+
+
+
+class ProfilePic extends React.Component{
+    render(){
+        return(
+            <img src={this.props.tweet.user.profile_image_url}/>
+    )};
+};
+class TweetName extends React.Component{
+    render(){
+        let date = this.props.tweet.created_at.substring(4,10);
+        if(date.charAt(4) === '0'){
+            date = date.replace('0', '');
+        }
+        return(
+            <p>{this.props.tweet.user.name} {this.props.tweet.user.screen_name} - {date}</p>
     )};
 };
 class TweetText extends React.Component{
     render(){
-        console.log(this.props.tweet)
         return(
             <p>{this.props.tweet.text}</p>
+    )};
+};
+
+class RetweetCount extends React.Component{
+    render(){
+        return(
+            <span>{this.props.tweet.retweet_count}</span>
+    )};
+};
+
+class FavoriteCount extends React.Component{
+    render(){
+        return(
+            <span>{this.props.tweet.favorite_count}</span>
     )};
 };
 
@@ -20,12 +64,18 @@ class TweetList extends React.Component{
         let tweet = this.props.tweets.map((tweet, index) => {
             return(
             <div>
+                <ProfilePic tweet={tweet}/>
                 <TweetName tweet={tweet}/>
                 <TweetText tweet={tweet}/>
+                <RetweetCount tweet={tweet}/> <FavoriteCount tweet={tweet}/>
             </div>
         )});
         return(
-            <div>{tweet}</div>
+            <div>
+                <UserHeader user={tweets[0].user}/>
+                <UserBanner user={tweets[0].user}/>
+                <div>{tweet}</div>
+            </div>
     )};
 };
 
